189ece363eb903a651bc65453066314455059fa9
giovanni <giovanni_ge@hotmai.com>
2023-02-04 10:40:11 -0300

    feat: adicionando logica de click

--- cart-commerce/src/pages/Main/Main.tsx
+++ cart-commerce/src/pages/Main/Main.tsx
@@ -7,16 +7,29 @@ export interface IListProducts {
   title: string
   thumbnail: string
   price: number
+  obj: string
 }
 
 export function Main() {
   const [listProduct, setListProduct] = useState<IListProducts[]>([]);
+  const [cart, setCart] = useState<IListProducts[]>([]);
   
   const fetchtList = async () => {
     const url = "https://api.mercadolibre.com/sites/MLB/search?q=celular";
     const response = await fetch(url);
     const objListJson = await response.json();
     setListProduct(objListJson.results)
+  }
+
+  const handleClick = (obj: IListProducts) => {
+    const elementVar = cart.find((element) => element.id === obj.id);
+    if(elementVar) {
+      const arrFilter = cart.filter((elementVew) => elementVew.id !== obj.id)
+      console.log("arrfilter", arrFilter)
+      setCart(arrFilter)
+    } else {
+      setCart([...cart, obj])
+    }
   }
 
   useEffect(() => {
@@ -32,8 +45,12 @@ export function Main() {
               <h4>{item.title}</h4>
               <img src={item.thumbnail} alt="" />
               <strong>{item.price}</strong>
-              <button>
-                <BsFillCartCheckFill />
+              <button onClick={() => handleClick(item)}>
+                {cart.some((itemCart) => itemCart.id === item.id) ? (
+                  <BsFillCartCheckFill />
+                ): (
+                  <BsFillCartPlusFill />
+                ) }
               </button>
             </CardList>
           )
